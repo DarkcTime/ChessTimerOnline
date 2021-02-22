57140bebc4d24558f5d4a484a9eca87bccf1d965
Vlad <vlad_chekmarev_01@mail.ru>
2021-02-22 20:16:51 +0300

    Add project files.

---
+++ ChessTimerOnline/Controllers/ApiRoom.cs
@@ -0,0 +1,61 @@
+﻿using ChessTimerOnline.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChessTimerOnline.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiRoom : ControllerBase
+    {
+        public static List<Room> ListRooms = new List<Room>();
+
+        private Room GetRoomFromList(int id)
+        {
+            return ListRooms.Where(i => i.Id == id).FirstOrDefault();
+        }
+
+        // GET: api/<ApiRoom>/
+        [HttpGet]
+        public List<Room> GetRooms()
+        {
+            return ListRooms; 
+        }
+
+        // GET: api/<ApiRoom>/GetRoom/5
+        [HttpGet("{id}")]
+        public Room GetRoom(int id)
+        {
+            return GetRoomFromList(id);
+        }
+
+        // POST api/<ApiRoom>/
+        [HttpPost]
+        public void AddRoom(Room room)
+        {
+            ListRooms.Add(room);
+        }
+
+        // PUT api/<ApiRoom>/1
+        [HttpPut("{id}")]
+        public void ChangeTimeUpdate(int id, Room room)
+        {
+            Room roomFind = GetRoomFromList(id);
+            int index = ListRooms.IndexOf(roomFind);
+            ListRooms[index].TimePlayer1 = room.TimePlayer1;
+        }
+
+        // DELETE api/<ApiRoom>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Room room = GetRoomFromList(id);
+            ListRooms.Remove(room);
+        }
+    }
+}
---
+++ ChessTimerOnline/Model/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessTimerOnline.Model
+{
+    public class Room
+    {
+        /// <summary>
+        /// For time use seconds
+        /// </summary>
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Player1 { get; set; }
+        public int TimePlayer1 { get; set; }
+        public string Player2 { get; set; }
+        public int TimePlayer2 { get; set; }
+        public int AddTime { get; set; }
+
+    }
+}
